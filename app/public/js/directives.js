1b58e853754e7933caf072a7cd697faea0f03566
Robert Phelps <rphelps@42six.com>
2013-08-27 17:14:03 -0400

    added directive for loading map from partial

--- app/public/js/directives.js
+++ app/public/js/directives.js
@@ -3,14 +3,23 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }])
-    .directive('dropdown', function () {
-    return function (scope, elm, attrs) {
-         $(elm).dropdown();
-        };
-    });
+angular.module('myApp.directives', []).directive('appVersion', ['version', function(version) {
+	return function(scope, elm, attrs) {
+		elm.text(version);
+	};
+}]).directive('dropdown', function() {
+	return function(scope, elm, attrs) {
+		$(elm).dropdown();
+	};
+}).directive('loadMapDefaults', function() {
+	return {
+	    link: function(scope, element, attrs) {
+			var mapOptions = {
+				center: new google.maps.LatLng(39.183917, -76.805643),
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				zoom: 15
+			};	
+			var map = new google.maps.Map((element || [])[0], mapOptions);
+	    }		
+	};
+});
